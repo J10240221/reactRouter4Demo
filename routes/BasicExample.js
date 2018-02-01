b12f13405d143165fc85a082a185208593ade967
zhiLinGeGe <zhilingege@gmail.com>
2018-02-01 15:38:42 +0800

    完成官网的第一个案例【Basic】

---
+++ routes/BasicExample.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const BasicExample = () => {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/topics">Topics</Link></li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </div>
+    </Router>
+  );
+}
+
+const Home = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topic</h2>
+    <div>
+      <ul>
+        <li><Link to={`${match.path}/topic1`}> topic1</Link></li>
+        <li><Link to={`${match.path}/topic2`}> topic2</Link></li>
+        <li><Link to={`${match.path}/topic3`}> topic3</Link></li>
+      </ul>
+      <Route path={`${match.path}/:topicNum`} component={Topic} />
+      <Route path={`${match.path}`} exact render={() => {
+        return (<div>Please select a topic</div>);
+      }} />
+    </div>
+  </div>
+);
+
+const Topic = ({ match }) => {
+  console.log(match.params);
+  return (
+    <div>
+      {match.params.topicNum}
+    </div>
+  );
+};
+
+export default BasicExample;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
+import BasicExample from '../routes/BasicExample';
 
 const styles = {
   fontFamily: 'sans-serif',
-  textAlign: 'center',
 };
 
 const App = () => (
   <div style={styles}>
-    <Hello name="CodeSandbox" />
-    <h2>Start editing to see some magic happen {'\u2728'}</h2>
+    <BasicExample />
   </div>
 );
 
